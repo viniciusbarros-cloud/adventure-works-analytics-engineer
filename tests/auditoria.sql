0816089f25b34ebe28f58592c5522008ac58ece6
Vinícius Barros da Silva <viniciusx10@yahoo.com.br>
2026-07-31 15:38:21 +0000

    Remoção do teste de auditoria antigo.

--- tests/auditoria.sql
+++
@@ -1,8 +0,0 @@
-select
-    extract(year from order_date) as ano,
-    round(sum(line_total), 2) as valor_calculado,
-    12646112.16 as valor_esperado,
-    round(sum(line_total), 2) - 12646112.16 as diferenca
-from dbt_vini.fct_sales -- ajuste para o seu schema se necessário
-where extract(year from order_date) = 2011
-group by 1
